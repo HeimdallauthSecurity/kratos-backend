25afad0b3e97098169bb7cbe29bf893fbbf4c1e2
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-05 21:02:25 +0530

    improve(PasswordResetPayload): KRAT-19 Lombok changes

--- src/main/java/com/heimdallauth/auth/kratosbackend/documents/PasswordResetRequests.java
+++ src/main/java/com/heimdallauth/auth/kratosbackend/documents/PasswordResetRequests.java
@@ -1,6 +1,10 @@
 package com.heimdallauth.auth.kratosbackend.documents;
 
 import com.heimdallauth.auth.kratosbackend.constants.PasswordResetStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 
 @Document
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PasswordResetRequests {
     @Id
     private String requestId;
